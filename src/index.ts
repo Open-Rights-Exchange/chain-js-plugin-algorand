bb22afc5a2ddc06d5dc80196cded70b179e6aebf
Warrick FitzGerald <warrick@aikon.com>
2022-05-20 08:20:44 -0400

    More export for use via shared

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as HelpersAlgorand from './plugin/helpers'
 import * as ModelsAlgorand from './plugin/models'
 import * as TemplateModelsAlgorand from './plugin/templates/models'
 import Plugin from './plugin/ChainAlgorandV1'
+import { AlgorandAccount, AlgorandTransaction } from './plugin'
 
-export { HelpersAlgorand, ModelsAlgorand, TemplateModelsAlgorand, Plugin }
+export { HelpersAlgorand, ModelsAlgorand, TemplateModelsAlgorand, AlgorandAccount, AlgorandTransaction, Plugin }
